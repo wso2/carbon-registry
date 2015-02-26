b7c4dcba7501e8f8bd33af5bf579e978d1df188f
Madawa Soysa <madawa.rc@gmail.com>
2015-02-26 17:18:52 +0530

    import from url

--- components/registry/org.wso2.carbon.registry.extensions/src/main/java/org/wso2/carbon/registry/extensions/handlers/SwaggerMediaTypeHandler.java
+++ components/registry/org.wso2.carbon.registry.extensions/src/main/java/org/wso2/carbon/registry/extensions/handlers/SwaggerMediaTypeHandler.java
@@ -23,23 +23,20 @@ import org.wso2.carbon.registry.common.utils.artifact.manager.ArtifactManager;
 import org.wso2.carbon.registry.core.Registry;
 import org.wso2.carbon.registry.core.RegistryConstants;
 import org.wso2.carbon.registry.core.Resource;
-import org.wso2.carbon.registry.core.ResourceImpl;
 import org.wso2.carbon.registry.core.config.RegistryContext;
 import org.wso2.carbon.registry.core.exceptions.RegistryException;
 import org.wso2.carbon.registry.core.jdbc.handlers.Handler;
 import org.wso2.carbon.registry.core.jdbc.handlers.RequestContext;
 import org.wso2.carbon.registry.core.utils.RegistryUtils;
 import org.wso2.carbon.registry.extensions.handlers.utils.SwaggerProcessor;
-import org.wso2.carbon.registry.extensions.utils.CommonConstants;
 import org.wso2.carbon.registry.extensions.utils.CommonUtil;
 
 import javax.xml.namespace.QName;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.*;
+import java.util.Iterator;
 
 public class SwaggerMediaTypeHandler extends Handler {
 
@@ -108,7 +105,7 @@ public class SwaggerMediaTypeHandler extends Handler {
 			Object newContent = RegistryUtils.encodeString(resourceContent);
 			if (newContent != null) {
 				InputStream inputStream = new ByteArrayInputStream((byte[])newContent);
-				addSwaggerToRegistry(requestContext, inputStream);
+				//addSwaggerToRegistry(requestContext, inputStream);
 			}
 			ArtifactManager.getArtifactManager().getTenantArtifactRepository().addArtifact(path);
 		} finally {
@@ -141,124 +138,13 @@ public class SwaggerMediaTypeHandler extends Handler {
 			} catch (IOException e) {
 				throw new RegistryException("The URL " + sourceURL + " is incorrect.", e);
 			}
-
-			addSwaggerToRegistry(requestContext, inputStream);
-			new SwaggerProcessor(requestContext).populateAPI(null,requestContext);
+			SwaggerProcessor processor = new SwaggerProcessor();
+			processor.addSwaggerToRegistry(requestContext, inputStream);
 			requestContext.setProcessingComplete(true);
 
 		} finally {
 			CommonUtil.releaseUpdateLock();
 		}
-
-	}
-
-	private ByteArrayOutputStream readSourceContent(InputStream inputStream)
-			throws RegistryException {
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		int nextChar;
-		try {
-			while ((nextChar = inputStream.read()) != -1) {
-				outputStream.write(nextChar);
-			}
-			outputStream.flush();
-		} catch (IOException e) {
-			throw new RegistryException("Exception occurred while reading swagger content", e);
-		}
-
-		return  outputStream;
-	}
-
-	private void addSwaggerToRegistry(RequestContext requestContext, InputStream inputStream)
-			throws RegistryException {
-
-		Resource resource;
-		if (requestContext.getResource() == null) {
-			resource = new ResourceImpl();
-			resource.setMediaType(CommonConstants.SWAGGER_MEDIA_TYPE);
-		} else {
-			resource = requestContext.getResource();
-		}
-
-		String version =
-				requestContext.getResource().getProperty(RegistryConstants.VERSION_PARAMETER_NAME);
-
-		if (version == null) {
-			version = CommonConstants.SWAGGER_VERSION_DEFAULT_VALUE;
-			resource.setProperty(RegistryConstants.VERSION_PARAMETER_NAME, version);
-		}
-
-		ByteArrayOutputStream resourceContent = readSourceContent(inputStream);
-
-
-		String resourcePath = requestContext.getResourcePath().getPath();
-		String swaggerFileName = resourcePath
-				.substring(resourcePath.lastIndexOf(RegistryConstants.PATH_SEPARATOR) + 1);
-
-		Registry systemRegistry = CommonUtil.getUnchrootedSystemRegistry(requestContext);
-		RegistryContext registryContext = requestContext.getRegistryContext();
-		String commonLocation = getChrootedLocation(registryContext);
-
-		//Creating new collection
-		if (!systemRegistry.resourceExists(commonLocation)) {
-			systemRegistry.put(commonLocation, systemRegistry.newCollection());
-		}
-
-		String swaggerPath;
-		if (!resourcePath.startsWith(commonLocation)
-		    && !resourcePath.equals(RegistryUtils.getAbsolutePath(registryContext,
-		                                                          RegistryConstants.PATH_SEPARATOR +
-		                                                          swaggerFileName))
-		    && !resourcePath.equals(RegistryUtils.getAbsolutePath(registryContext,
-		                                                          RegistryConstants.GOVERNANCE_REGISTRY_BASE_PATH +
-		                                                          RegistryConstants.PATH_SEPARATOR + swaggerFileName))) {
-			swaggerPath = resourcePath;
-		} else {
-			swaggerPath = commonLocation + version + "/" + swaggerFileName;
-		}
-
-
-		Registry registry = requestContext.getRegistry();
-
-		String relativeArtifactPath = RegistryUtils.getRelativePath(registry.getRegistryContext(), swaggerPath);
-
-		relativeArtifactPath = RegistryUtils.getRelativePathToOriginal(relativeArtifactPath,
-		                                                               RegistryConstants.GOVERNANCE_REGISTRY_BASE_PATH);
-
-		Resource newResource;
-		if (registry.resourceExists(swaggerPath)) {
-			newResource = registry.get(swaggerPath);
-		} else {
-			newResource = new ResourceImpl();
-			Properties properties = resource.getProperties();
-			if (properties != null) {
-				List<String> linkProperties = Arrays
-						.asList(RegistryConstants.REGISTRY_LINK, RegistryConstants.REGISTRY_USER,
-						        RegistryConstants.REGISTRY_MOUNT, RegistryConstants.REGISTRY_AUTHOR,
-						        RegistryConstants.REGISTRY_MOUNT_POINT,
-						        RegistryConstants.REGISTRY_TARGET_POINT,
-						        RegistryConstants.REGISTRY_ACTUAL_PATH,
-						        RegistryConstants.REGISTRY_REAL_PATH);
-				for (Map.Entry<Object, Object> e : properties.entrySet()) {
-					String key = (String) e.getKey();
-					if (!linkProperties.contains(key)) {
-						newResource.setProperty(key, (List<String>) e.getValue());
-					}
-				}
-			}
-		}
-
-		newResource.setMediaType(CommonConstants.SWAGGER_MEDIA_TYPE);
-		String swaggerResourceUUID = resource.getUUID();
-		if (swaggerResourceUUID == null) {
-			swaggerResourceUUID = UUID.randomUUID().toString();
-		}
-		newResource.setUUID(swaggerResourceUUID);
-		newResource.setContent(new String(resourceContent.toByteArray()));
-		addSwaggerToRegistry(requestContext, swaggerPath, requestContext.getSourceURL(),
-		                     newResource, registry);
-		((ResourceImpl)newResource).setPath(relativeArtifactPath);
-
-		requestContext.setResource(newResource);
 
 	}
 
@@ -268,9 +154,4 @@ public class SwaggerMediaTypeHandler extends Handler {
 		                                     location);
 	}
 
-	protected void addSwaggerToRegistry(RequestContext context, String path, String url,
-	                                    Resource resource, Registry registry) throws RegistryException {
-		context.setActualPath(path);
-		registry.put(path, resource);
-	}
 }
--- components/registry/org.wso2.carbon.registry.extensions/src/main/java/org/wso2/carbon/registry/extensions/handlers/utils/SwaggerProcessor.java
+++ components/registry/org.wso2.carbon.registry.extensions/src/main/java/org/wso2/carbon/registry/extensions/handlers/utils/SwaggerProcessor.java
@@ -17,63 +17,137 @@
 package org.wso2.carbon.registry.extensions.handlers.utils;
 
 import org.wso2.carbon.registry.core.Registry;
+import org.wso2.carbon.registry.core.RegistryConstants;
 import org.wso2.carbon.registry.core.Resource;
 import org.wso2.carbon.registry.core.ResourceImpl;
-import org.wso2.carbon.registry.core.ResourcePath;
+import org.wso2.carbon.registry.core.config.RegistryContext;
 import org.wso2.carbon.registry.core.exceptions.RegistryException;
-import org.wso2.carbon.registry.core.jdbc.Repository;
-import org.wso2.carbon.registry.core.jdbc.VersionRepository;
 import org.wso2.carbon.registry.core.jdbc.handlers.RequestContext;
 import org.wso2.carbon.registry.core.utils.RegistryUtils;
+import org.wso2.carbon.registry.extensions.utils.CommonConstants;
+import org.wso2.carbon.registry.extensions.utils.CommonUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 
 public class SwaggerProcessor {
 
-	private Registry registry;
-	private Repository repository;
-	private VersionRepository versionRepository;
+	private ByteArrayOutputStream readSourceContent(InputStream inputStream)
+			throws RegistryException {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		int nextChar;
+		try {
+			while ((nextChar = inputStream.read()) != -1) {
+				outputStream.write(nextChar);
+			}
+			outputStream.flush();
+		} catch (IOException e) {
+			throw new RegistryException("Exception occurred while reading swagger content", e);
+		}
 
-	public SwaggerProcessor(RequestContext requestContext) {
-		registry = requestContext.getRegistry();
-		repository = requestContext.getRepository();
-		versionRepository = requestContext.getVersionRepository();
+		return  outputStream;
 	}
 
-	public void populateAPI(String swaggerContent, RequestContext requestContext)
+	public void addSwaggerToRegistry(RequestContext requestContext, InputStream inputStream)
 			throws RegistryException {
 
-		Resource resource = new ResourceImpl();
-		resource.setMediaType("application/vnd.wso2-restapi");
-		String s = "<metadata xmlns=\"http://www.wso2.org/governance/metadata\">" +
-		           "<information>" +
-		           "<title>Test</title>" +
-		           "<description>testing description</description>" +
-		           "<host>http://localhost</host>" +
-		           "<basePath>/petstore</basePath>" +
-		           "<version>1.0.0</version>" +
-		           "</information>" +
-		           "<contact>" +
-		           "<name>Test</name>" +
-		           "<url>http://localhost</url>" +
-		           "<email>http://localhost</email>" +
-		           "</contact>" +
-		           "<licence>" +
-		           "<name>Apache</name>" +
-		           "<url>http://localhost</url>" +
-		           "</licence>" +
-		           "<interface>" +
-		           "<swaggerURL>http://localhost</swaggerURL>" +
-		           "<schemes>https</schemes>" +
-		           "<consumes>application/json</consumes>" +
-		           "<produces>text/plain</produces>" +
-		           "</interface>" +
-		           "</metadata>";
+		Resource resource;
+		if (requestContext.getResource() == null) {
+			resource = new ResourceImpl();
+			resource.setMediaType(CommonConstants.SWAGGER_MEDIA_TYPE);
+		} else {
+			resource = requestContext.getResource();
+		}
 
-		resource.setContent(RegistryUtils.encodeString(s));
+		String version =
+				requestContext.getResource().getProperty(RegistryConstants.VERSION_PARAMETER_NAME);
+		String apiName =
+				requestContext.getResource().getProperty("apiName");
 
-		String actualPath = "/_system/governance/apimgt/applicationdata/Test2/1.2.0/api";
-		requestContext.setResourcePath(new ResourcePath(actualPath));
-		requestContext.setResource(resource);
-		registry.put(actualPath, resource);
+		if (version == null) {
+			version = CommonConstants.SWAGGER_VERSION_DEFAULT_VALUE;
+			resource.setProperty(RegistryConstants.VERSION_PARAMETER_NAME, version);
+		}
+
+		ByteArrayOutputStream resourceContent = readSourceContent(inputStream);
+
+		String resourcePath = requestContext.getResourcePath().getPath();
+		String swaggerFileName = resourcePath
+				.substring(resourcePath.lastIndexOf(RegistryConstants.PATH_SEPARATOR) + 1);
+
+		Registry systemRegistry = CommonUtil.getUnchrootedSystemRegistry(requestContext);
+		RegistryContext registryContext = requestContext.getRegistryContext();
+		String commonLocation = "";//getChrootedLocation(registryContext);
+
+		//Creating new collection
+		if (!systemRegistry.resourceExists(commonLocation)) {
+			systemRegistry.put(commonLocation, systemRegistry.newCollection());
+		}
+
+		String swaggerPath;
+		if (!resourcePath.startsWith(commonLocation)
+		    && !resourcePath.equals(RegistryUtils.getAbsolutePath(registryContext,
+		                                                          RegistryConstants.PATH_SEPARATOR +
+		                                                          swaggerFileName))
+		    && !resourcePath.equals(RegistryUtils.getAbsolutePath(registryContext,
+		                                                          RegistryConstants.GOVERNANCE_REGISTRY_BASE_PATH +
+		                                                          RegistryConstants.PATH_SEPARATOR + swaggerFileName))) {
+			swaggerPath = resourcePath;
+		} else {
+			swaggerPath = commonLocation + version + "/" + swaggerFileName;
+		}
+
+
+		Registry registry = requestContext.getRegistry();
+
+		String relativeArtifactPath = RegistryUtils.getRelativePath(registry.getRegistryContext(), swaggerPath);
+
+		relativeArtifactPath = RegistryUtils.getRelativePathToOriginal(relativeArtifactPath,
+		                                                               RegistryConstants.GOVERNANCE_REGISTRY_BASE_PATH);
+
+		Resource newResource;
+		if (registry.resourceExists(swaggerPath)) {
+			newResource = registry.get(swaggerPath);
+		} else {
+			newResource = new ResourceImpl();
+			Properties properties = resource.getProperties();
+			if (properties != null) {
+				List<String> linkProperties = Arrays
+						.asList(RegistryConstants.REGISTRY_LINK, RegistryConstants.REGISTRY_USER,
+						        RegistryConstants.REGISTRY_MOUNT, RegistryConstants.REGISTRY_AUTHOR,
+						        RegistryConstants.REGISTRY_MOUNT_POINT,
+						        RegistryConstants.REGISTRY_TARGET_POINT,
+						        RegistryConstants.REGISTRY_ACTUAL_PATH,
+						        RegistryConstants.REGISTRY_REAL_PATH);
+				for (Map.Entry<Object, Object> e : properties.entrySet()) {
+					String key = (String) e.getKey();
+					if (!linkProperties.contains(key)) {
+						newResource.setProperty(key, (List<String>) e.getValue());
+					}
+				}
+			}
+		}
+
+		newResource.setMediaType(CommonConstants.SWAGGER_MEDIA_TYPE);
+		String swaggerResourceUUID = resource.getUUID();
+		if (swaggerResourceUUID == null) {
+			swaggerResourceUUID = UUID.randomUUID().toString();
+		}
+		newResource.setUUID(swaggerResourceUUID);
+		newResource.setContent(new String(resourceContent.toByteArray()));
+		addSwaggerToRegistry(requestContext, swaggerPath, requestContext.getSourceURL(),
+		                     newResource, registry);
+		((ResourceImpl)newResource).setPath(relativeArtifactPath);
+
+		requestContext.setResource(newResource);
+
 	}
 
+	protected void addSwaggerToRegistry(RequestContext context, String path, String url,
+	                                    Resource resource, Registry registry) throws RegistryException {
+		context.setActualPath(path);
+		registry.put(path, resource);
+	}
 }
