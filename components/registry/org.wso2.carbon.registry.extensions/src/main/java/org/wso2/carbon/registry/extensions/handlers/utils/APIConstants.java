ecec5fab9cf0622fb8ba34439b6928eaa520bf0f
Madawa Soysa <madawa.rc@gmail.com>
2015-03-12 15:02:32 +0530

    constants

---
+++ components/registry/org.wso2.carbon.registry.extensions/src/main/java/org/wso2/carbon/registry/extensions/handlers/utils/APIConstants.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.registry.extensions.handlers.utils;
+
+public class APIConstants {
+
+	//Swagger Constants
+	public static final String SWAGGER_VERSION_2 = "2.0";
+	public static final String SWAGGER_VERSION_1_2 = "1.2";
+
+	public static final String INFO_OBJECT = "info";
+	public static final String TITLE = "title";
+	public static final String DESCRIPTION = "description";
+	public static final String API_VERSION = "apiVersion";
+	public static final String SWAGGER_VERSION = "swaggerVersion";
+
+	public static final String APIS = "apis";
+	public static final String HOST = "host";
+	public static final String BASEPATH = "basepath";
+	public static final String SCHEMES = "schemes";
+	public static final String NAME = "name";
+	public static final String PATH = "path";
+	public static final String OPERATIONS = "operations";
+	public static final String PARAMETERS = "parameters";
+	public static final String SUMMARY = "summary";
+	public static final String METHOD = "method";
+	public static final String PARAM_TYPE = "paramType";
+	public static final String TYPE = "type";
+	public static final String REQUIRED = "required";
+	public static final String RESOURCE_PATH = "resourcePath";
+	public static final String PATHS = "paths";
+
+}
