d3ba02a854e968f1ad5f8d0f43f43a7902dd96b7
Malintha Fernando <malinthafe@wso2.com>
2016-01-28 17:18:41 +0530

    Add basic methods to MetadataDAO interface

--- org.wso2.carbon.metadata.server/src/main/java/org/wso2/carbon/metadata/server/dao/MetadataDAO.java
+++ org.wso2.carbon.metadata.server/src/main/java/org/wso2/carbon/metadata/server/dao/MetadataDAO.java
@@ -15,8 +15,55 @@
  */
 package org.wso2.carbon.metadata.server.dao;
 
+import org.wso2.carbon.metadata.server.api.Collection;
+import org.wso2.carbon.metadata.server.api.Key;
+import org.wso2.carbon.metadata.server.api.MetadataStoreException;
+import org.wso2.carbon.metadata.server.impl.ResourceImpl;
+
 /**
  * Metadata DAO
  */
 public interface MetadataDAO {
+    /**
+     * Method to add a resource or collection to a given path
+     *
+     * @param resourceImpl Implementation of the resource or collection that needs to be added
+     * @return the resource id from the table
+     * @throws MetadataStoreException throws if operation is failed
+     */
+    void add(ResourceImpl resourceImpl) throws MetadataStoreException;
+
+    /**
+     * Removes a resource or a collection from the table given the UUID
+     *
+     * @param uuid UUID of the resource that needs to be removed
+     * @throws MetadataStoreException throws if the operation failed
+     */
+    void remove(String uuid) throws MetadataStoreException;
+
+    /**
+     * Removes a collection or a resource given the path
+     *
+     * @param path
+     * @throws MetadataStoreException
+     */
+    void remove(Key path) throws MetadataStoreException;
+
+    /**
+     * Update a resource given the UUID
+     *
+     * @param uuid UUID of the resource
+     * @throws MetadataStoreException
+     */
+    void update(String uuid) throws MetadataStoreException;
+
+    /**
+     * get the list of paths of children of a collection
+     *
+     * @param collection the collection needs to be addressed
+     * @return list of paths
+     * @throws MetadataStoreException
+     */
+    String[] getChildren(Collection collection) throws MetadataStoreException;
+
 }
--- org.wso2.carbon.metadata.server/src/main/java/org/wso2/carbon/metadata/server/dao/inmemory/InMemoryMetadataDAO.java
+++ org.wso2.carbon.metadata.server/src/main/java/org/wso2/carbon/metadata/server/dao/inmemory/InMemoryMetadataDAO.java
@@ -1,9 +1,32 @@
 package org.wso2.carbon.metadata.server.dao.inmemory;
 
+import org.wso2.carbon.metadata.server.api.Collection;
+import org.wso2.carbon.metadata.server.api.Key;
+import org.wso2.carbon.metadata.server.api.MetadataStoreException;
 import org.wso2.carbon.metadata.server.dao.MetadataDAO;
+import org.wso2.carbon.metadata.server.impl.ResourceImpl;
 
 /**
  * In Memory Metadata DAO
  */
 public class InMemoryMetadataDAO implements MetadataDAO {
+    @Override public void add(ResourceImpl resourceImpl) throws MetadataStoreException {
+        
+    }
+
+    @Override public void remove(String uuid) throws MetadataStoreException {
+
+    }
+
+    @Override public void remove(Key path) throws MetadataStoreException {
+
+    }
+
+    @Override public void update(String uuid) throws MetadataStoreException {
+
+    }
+
+    @Override public String[] getChildren(Collection collection) throws MetadataStoreException {
+        return new String[0];
+    }
 }
