04a02fedb63fa8b9d348654f4491649fc6cb1e57
Lahiru J Ekanayake <lahirue@wso2.com>
2016-04-11 16:31:45 +0530

    taxonomy adding formating and license

--- components/registry/org.wso2.carbon.registry.extensions/src/main/java/org/wso2/carbon/registry/extensions/handlers/TaxonomyResourceHandler.java
+++ components/registry/org.wso2.carbon.registry.extensions/src/main/java/org/wso2/carbon/registry/extensions/handlers/TaxonomyResourceHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 20016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,22 +15,18 @@
  */
 
 package org.wso2.carbon.registry.extensions.handlers;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.w3c.dom.Document;
 import org.wso2.carbon.registry.core.exceptions.RegistryException;
 import org.wso2.carbon.registry.core.jdbc.handlers.Handler;
 import org.wso2.carbon.registry.core.jdbc.handlers.RequestContext;
-import org.w3c.dom.Document;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.wso2.carbon.registry.extensions.services.TaxonomyStorageService;
 import org.wso2.carbon.registry.extensions.services.Utils;
 import org.xml.sax.SAXException;
 
+import java.io.IOException;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -48,27 +44,12 @@ public class TaxonomyResourceHandler extends Handler {
                 Utils.setTaxonomyService(ins);
             }
         } catch (SAXException e) {
-            log.info(e.getMessage());
+            log.error("SAX Exception occurred", e);
         } catch (IOException e) {
-            log.info(e.getMessage());
+            log.error("IO Exception occurred", e);
         } catch (ParserConfigurationException e) {
-            log.info(e.getMessage());
+            log.error("Parser configuration Exception occurred", e);
         }
 
-    }
-
-    @Override public String rename(RequestContext requestContext) throws RegistryException {
-        log.info("------------Renamed---------------- Not Implemented yet");
-        return super.rename(requestContext);
-    }
-
-    @Override public String copy(RequestContext requestContext) throws RegistryException {
-        log.info("------------Copied---------------- Not Implemented yet");
-        return super.copy(requestContext);
-    }
-
-    @Override public String move(RequestContext requestContext) throws RegistryException {
-        log.info("------------Moved---------------- Not Implemented yet");
-        return super.move(requestContext);
     }
 }
